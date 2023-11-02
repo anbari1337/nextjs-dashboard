65f44f80aeea305cb64844514b7f7d434adaf2bf
Amine Anbari <amine.a@wiggli.io>
2023-11-02 11:50:07 +0100

    adding authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,37 +4,78 @@ import { z } from "zod";
 import { v4 as uuid } from "uuid";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin";
+    }
+    throw error;
+  }
+}
 
 const InvoiceSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-export async function createInvoide(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoide(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
+  const { amount, customerId, status } = validatedFields.data;
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  const invoice = await prisma.invoice.create({
-    data: {
-      id: uuid(),
-      customerId,
-      status,
-      amount: amountInCents,
-      date,
-    },
-  });
-
+  try {
+    const invoice = await prisma.invoice.create({
+      data: {
+        id: uuid(),
+        customerId,
+        status,
+        amount: amountInCents,
+        date,
+      },
+    });
+  } catch (error) {
+    return { message: "Database Error: Failed to Create Invoice." };
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -50,27 +91,35 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  const result = await prisma.invoice.update({
-    where: {
-      id,
-    },
-    data: {
-      customerId,
-      amount: amountInCents,
-      status,
-    },
-  });
-
+  try {
+    const result = await prisma.invoice.update({
+      where: {
+        id,
+      },
+      data: {
+        customerId,
+        amount: amountInCents,
+        status,
+      },
+    });
+  } catch (error) {
+    return { message: "Database Error: Failed to Update Invoice." };
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  const result = await prisma.invoice.delete({
-    where: {
-      id,
-    },
-  });
+  try {
+    const result = await prisma.invoice.delete({
+      where: {
+        id,
+      },
+    });
+    revalidatePath("/dashboard/invoices");
 
-  revalidatePath("/dashboard/invoices");
+    return { message: "Invoice Deleted" };
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
+  }
 }
